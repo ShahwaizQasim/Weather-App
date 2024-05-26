184100debfd2641eb64aa7beca2647e06c1fe0cc
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-05-25 22:59:24 -0700

    20% work complete

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 
-let myinput = document.querySelector("#my-input");
 let form = document.querySelector("#wheather-form");
-let screenparagraph = document.querySelector("#wheather-update");
+let myInput = document.querySelector("#my-input");
+const API_KEY = "0cddb8b14288b6d5d30fc7f9c60711ea";
+
+/* Api data ko show krwany ky liye kuch element ko select kiya hai */
+let temp = document.querySelector("#citytemp");
+let humidity = document.querySelector("#cityhumidity");
+let wind = document.querySelector("#citywind");
+let cityPrecipitation = document.querySelector("#city_precipitation");
+let cityFeels = document.querySelector("#city_feelslike");
 //let mybutton = document.querySelector("#my-button");
 
-form.addEventListener("submit", (event) => {
+ const formHandler = async (event) => {
 
    event.preventDefault(); //is se page referesh nhi hota
 
-   let temperature = myinput.value;
-   let conditions = temperature < 20;
+   let city_temperature = myInput.value;
 
-    if(conditions){
-        screenparagraph.innerText = "Today Wheater Is Cold" 
-    }
-    else{
-        screenparagraph.innerText = "Today Wheater Is Hot"
-    }
-});
+   const response = await axios( `https://api.openweathermap.org/data/2.5/weather?q=${city_temperature}&appid=${API_KEY}&units=metric`);
+   console.log(response);
+};
+
+form.addEventListener("submit", formHandler)
